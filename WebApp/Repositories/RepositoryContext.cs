fe0a6f62f932eaa55246a74deed3775b4905543b
yigityucel48 <yigityucel48@gmail.com>
2024-10-02 02:17:55 +0300

    first commit

---
+++ WebApp/Repositories/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Repositories
+{
+    public class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions options):
+            base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
